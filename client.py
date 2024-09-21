1b2e2ffa04b9bf1f17f5589ca9335c9e5bbe9af5
hsspedro <pedrohss1604@outlook.com>
2024-09-21 20:11:55 -0300

    'TimeOut'

--- client.py
+++ client.py
@@ -1,11 +1,28 @@
 import socket
+import time
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
+
 def send():
     i = input("Digite a mensagem para ser enviada: ")
     checksum = bytes(i, encoding="ASCII")
+    time.sleep(30)
     s.send(bytes(f"Mensagem de hoje: {i}", "utf-8"))
-    bytes()
 
+def timer():
+    mustend = time.time() + 5
+    while True:
+        start_time = time.time()
+        send()
+        while time.time() < mustend:
+            try:
+                response = s.recv(1024)
+                if response == b"ack":
+                    print("tudo certo")
+                    break
+            except socket.timeout:
+                print("nao foi possivel")
+                break
 
-send()
+timer()
--- server.py
+++ server.py
@@ -2,21 +2,33 @@ import socket
 import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-time = time.sleep(3)
-
+s.settimeout(5)  # Define um tempo limite para o socket
 
 def create():
     s.bind((socket.gethostname(), 1234))
     s.listen(5)
 
-
 def start():
     while True:
-        clientsocket, address = s.accept()
-        print(f"{address} se conectou ao servidor")
-        print(clientsocket.recv(1024))
-        clientsocket.close()
-            
+        try:
+            clientsocket, address = s.accept()
+            print(f"{address} se conectou ao servidor")
+            start_timer = time.time()
+            end_timer = start_timer + 30
+            try:
+                data = clientsocket.recv(1024)
+                if data and start_timer < end_timer:
+                    print("Recebi")
+                    clientsocket.send(bytes("ack", "utf-8"))
+                else:
+                    raise ValueError("No data received or timeout")
+            except (socket.timeout, ValueError):
+                print(f"{start_timer} Nao recebi")
+                clientsocket.send(bytes("NACK", "utf-8"))
+                clientsocket.send(bytes("Encerrei", "utf-8"))
+                clientsocket.close()
+        except socket.timeout:
+            print("Timeout ao tentar aceitar uma conexão. Tentando novamente...")
 
 create()
 start()
